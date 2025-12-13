85700e93fadf7a53987a21bdc74c01588816518c
WaveDev <wavedev@github.com>
2025-12-13 17:56:15 +0100

    idk but fix newlines

--- check.js
+++ check.js
@@ -25,11 +25,11 @@ for (let route of routes) {
 const mdify = (logs) => {
     const [covered, missing] = [logs.covered.length, logs.missing.length]
     const table = `| Covered | ${covered} |\n|---------|------------|\n| Missing | ${missing} |`
-    let res = "# Results\n\n" + table + "\n## Missing routes\ntotal: " + missing + "\n"
+    let res = "# Results\n\n" + table + "\n## Missing routes\ntotal: " + missing + "\n</br>"
     for (let item of logs.missing) {
         res += `- [ ] **${item[0]}**: \`${item[1]}\`\n`
     }
-    res += "\n## Covered routes\ntotal: " + covered + "\n"
+    res += "\n## Covered routes\ntotal: " + covered + "\n</br>"
     for (let item of logs.covered) {
         res += `- [x] **${item[0]}**: \`${item[1]}\`\n`
     }
